ac877c146e0dba7d2d46d1399969eecac1727565
Chaerul Umam <chaerulumamb244@gmail.com>
2023-08-17 12:52:06 +0700

    implement repository pattern, and modify db schema

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Repositories\UserRepositoryInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
     public function index()
     {
         return view("auth.register");
@@ -24,7 +31,7 @@ class RegisterController extends Controller
             'password' => 'required|min:6'
         ]);
 
-        User::create([
+        $this->userRepository->create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password)
