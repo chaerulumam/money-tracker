bed3d8fe2b3c06b0bcc4e75c32baf709f1b001ef
Chaerul Umam <chaerulumamb244@gmail.com>
2023-08-30 00:19:12 +0700

    feat: Create Category API Documentation

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthController extends Controller
+{
+    // 
+}
---
+++ app/Http/Controllers/api/CategoryController.php
@@ -0,0 +1,239 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use App\Repositories\CategoryRepositoryInterface;
+
+
+
+
+
+class CategoryController extends Controller
+{
+    protected $categoryRepository;
+
+    public function __construct(CategoryRepositoryInterface $categoryRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+    }
+
+    /**
+     * @OA\Schema(
+     *     schema="Category",
+     *     @OA\Property(property="id", type="integer"),
+     *     @OA\Property(property="title", type="string"),
+     *     @OA\Property(property="created_at", type="string", format="date-time"),
+     *     @OA\Property(property="updated_at", type="string", format="date-time")
+     * )
+     */
+
+    /**
+     * @OA\Get(
+     *     path="/api/categories",
+     *     summary="Get all categories",
+     *     tags={"Categories Data"},
+     *     @OA\Response(
+     *         response="200",
+     *         description="List of categories",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="object",
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="title", type="string"),
+     *                 @OA\Property(property="created_at", type="string", format="date-time"),
+     *                 @OA\Property(property="updated_at", type="string", format="date-time")
+     *             )
+     *         )
+     *     )
+     * )
+     * 
+     */
+    public function index()
+    {
+        $categories = $this->categoryRepository->getAllData();
+
+        return response()->json($categories, 200);
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/categories",
+     *     summary="Create a new category",
+     *     tags={"Create a new Category Data"},
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="title", type="string", example="New Category")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="Category created successfully",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="Bad request",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     )
+     * )
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255'
+        ]);
+
+        try {
+            $this->categoryRepository->create($data);
+            return response()->json(['message' => 'Category created successfully'], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error creating category'], 400);
+        }
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/category/{id}",
+     *     summary="Get a category by ID",
+     *     tags={"Category by ID"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the category",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Category details",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="id", type="integer"),
+     *             @OA\Property(property="title", type="string"),
+     *             @OA\Property(property="created_at", type="string", format="date-time"),
+     *             @OA\Property(property="updated_at", type="string", format="date-time")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Category not found",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     )
+     * )
+     */
+
+    public function show($id)
+    {
+        $category = $this->categoryRepository->findDataById($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        return response()->json($category, 200);
+    }
+
+    /**
+     * @OA\Put(
+     *     path="/api/category/{id}",
+     *     summary="Update a category by ID",
+     *     tags={"Category Update"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the category",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="title", type="string", example="Updated Category")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Category updated successfully",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Category not found",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     )
+     * )
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->validate([
+            'title' => 'required|string|max:255'
+        ]);
+
+        $category = $this->categoryRepository->updateDataById($id, $data);
+
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        return response()->json(['message' => 'Category updated successfully'], 200);
+    }
+
+    /**
+     * @OA\Delete(
+     *     path="/api/category/{id}",
+     *     summary="Delete a category by ID",
+     *     tags={"Category Delete"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="ID of the category",
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response="204",
+     *         description="Category deleted successfully"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Category not found",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string")
+     *         )
+     *     )
+     * )
+     */
+    public function destroy($id)
+    {
+        $category = $this->categoryRepository->findDataById($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        $this->categoryRepository->deleteDataById($id);
+
+        return response(null, 204);
+    }
+}
